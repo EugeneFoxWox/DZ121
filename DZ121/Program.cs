e747a6ea243bee4c3a2f51ae5fa76bb58ff82693
EugeneFoxWox <tereschonok.evgeniykpc742020@gmail.com>
2023-02-01 20:55:36 +0300

    Добавьте файлы проекта.

---
+++ DZ121/Program.cs
@@ -0,0 +1,17 @@
+﻿class Doma
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение первой переменной А: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите значение второй переменной B: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int tmp;
+        tmp = a; 
+        a = b;
+        b = tmp;
+
+        Console.WriteLine("Поздравляю, Вы успешно поменяли их значения!\nТеперь A = {0},\nпеременная B = {1}\n Конец программы!", a, b);
+    }
+}
